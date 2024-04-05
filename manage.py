fbb34fe5859f3772e3af0aae20afe096b3375359
Andres Huerfano <andresfelipehuerfano.99@gmail.com>
2024-04-05 12:03:35 -0500

    third

--- manage.py
+++ manage.py
@@ -2,6 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management.commands.runserver import Command as runserver
+
+runserver.default_port = os.environ.get('PORT', '8000')
+
 
 
 def main():
