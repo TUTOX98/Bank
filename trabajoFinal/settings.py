be971d2d6ab147803005bdd23fc8283aac1d5e6b
Andres Huerfano <andresfelipehuerfano.99@gmail.com>
2024-04-05 16:05:58 -0500

    static3

--- trabajoFinal/settings.py
+++ trabajoFinal/settings.py
@@ -19,8 +19,9 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     ...
 ]
+
 # This setting informs Django of the URI path from which your static files will be served to users
-# Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
+# Here, they will be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
 STATIC_URL = '/static/'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
@@ -40,20 +41,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5(9-1))-9n^h4j!a_jv380wyxc4hdkwn-im*)%ik2&yg*%aqw_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+DEBUG = True
+
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# settings.py
 
 ALLOWED_HOSTS = ['bank-1-b0fs.onrender.com', 'localhost', '127.0.0.1', '[::1]']
-
 
 
 # Application definition
@@ -68,15 +65,16 @@ INSTALLED_APPS = [
     'libreria'
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+# Remove the duplicated MIDDLEWARE definition
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
 
 ROOT_URLCONF = 'trabajoFinal.urls'
 
@@ -101,17 +99,6 @@ WSGI_APPLICATION = 'trabajoFinal.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'bank',
-#         'USER': 'admin',
-#         'PASSWORD': 'KevinCalderon123',
-#         'HOST': 'authobankk.crsgack00417.us-east-1.rds.amazonaws.com',
-#         'PORT': '3306',
-#     }
-# }
 
 DATABASES = {
     'default': {
@@ -161,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# No need to redefine STATIC_URL, it's already defined earlier
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
